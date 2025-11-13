d5c146f552560e65143ae0937a9a2081e8b2f3d2
oskarmmd <osde0001@stud.kea.dk>
2025-11-13 11:39:52 +0100

    Hero

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+
+const Hero = () => {
+    return ( 
+        <section className="relative h-[calc(100vh-4.625rem)] overflow-hidden">
+            <img 
+        src="https://dummyjson.com/image/1920x1080/282828"
+        alt="Demo image"
+        className="w-full h-full object-cover block"
+      />
+      <div className="absolute inset-0 flex flex-col items-start justify-end p-6 mx-24 mb-24 ">
+        <h1 className="text-white mb-6 text-7xl ">
+            Lorem Ipsum
+        </h1>
+        <Link href="/products">
+        <button className="bg-white p-2 pr-6 pl-6">
+            <h2>Se produkter</h2>
+        </button>
+        </Link>
+      </div>
+        </section>
+     );
+}
+ 
+export default Hero;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import Hero from "./components/Hero";
 
 
 export default function Home() {
   return (
     <div className="">
       <main className="">
+        <Hero />
       </main>
     </div>
   );
