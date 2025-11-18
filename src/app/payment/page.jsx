db684745ffdfe4ef668d8908926b78e58f9f578a
Julie <juth0001@stud.kea.dk>
2025-11-18 17:26:01 +0100

    payment page

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -1,8 +1,20 @@
 "use client";
+import { useEffect, useState } from "react";
 import Kurv from "../components/Kurv";
 import PayButton from "../components/PayButton";
 
 export default function PaymentPage() {
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const saved = localStorage.getItem("cartItems");
+    if (!saved) return;
+
+    const items = JSON.parse(saved);
+    const sum = items.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
+    setTotal(sum);
+  }, []);
   return (
     <main className="min-h-screen bg-[#FDFBF7]">
       <section className="max-w-3xl mx-auto mt-12 px-6">
@@ -10,7 +22,7 @@ export default function PaymentPage() {
         <Kurv paymentMode={true} />
 
         <div className="mt-24 text-center">
-          <p className="text-xl mb-6">I alt at betale: 200,-</p>
+          <p className="text-xl mb-6">I alt at betale:  {total},-</p>
           <PayButton>Betal nu</PayButton>
         </div>
       </section>
